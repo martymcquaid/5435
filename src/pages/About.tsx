c85b26049178bb6ea7242c8b922624fdc6d47eb1
martymcquaid <martymcquaid@users.noreply.github.com>
2025-11-30 20:59:24 +0000

    Add src/pages/About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-6">
+      <h1 className="text-5xl font-bold text-gray-800">About Pluimmers</h1>
+      <p className="text-xl text-gray-600 mt-4">We are a team of professionals dedicated to providing exceptional services in our field.</p>
+    </div>
+  );
+};
+
+export default About;
